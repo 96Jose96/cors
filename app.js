cbba307037dcc83e0578b6f328be546ee7c83c8e
Jose <jm.diaz6620@gmail.com>
2024-09-11 18:39:47 +0200

    back terminado

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
+
+const PORT = 3000;
+
+
+app.use(cors());
+
+
+const getAllCharacters = async () => {
+    let allCharacters = [];
+    let nextPageUrl = 'https://rickandmortyapi.com/api/character';
+
+    while (nextPageUrl) {
+        try {
+            const response = await axios.get(nextPageUrl);
+            allCharacters = allCharacters.concat(response.data.results); // Preguntar a Data porque da error sin .results
+
+            nextPageUrl = response.data.info.next;
+        } catch (error) {
+            res.status(404).json({error: 'No se pudo obtener la lista de personajes'});
+        }
+    }
+
+    return allCharacters;
+};
+
+
+app.get('/characters', async (req, res) => {
+    try {
+        const characters = await getAllCharacters();
+        const charactersList = characters.map(({name, status, species, gender, origin: {name: originName}, image}) => ({
+            name,
+            status,
+            species,
+            gender,
+            origin: {name: originName},
+            image
+        }));
+        
+        res.json(charactersList);
+    } catch (error) {
+        res.status(404).json({ error: 'No se puede mostrar lista de personajes' });
+    }
+});
+
+
+app.get('/characters/:name', async (req, res) => {
+    const characterName = req.params.name;
+
+    try {
+        const characters = await getAllCharacters();
+        const filterCharacters = characters.filter(char => char.name.toLowerCase().includes(characterName.toLowerCase()));
+        
+        if (matchedCharacters.length > 0) {
+            res.json(filterCharacters);
+        } else {
+            res.status(404).json({ error: 'Personaje no encontrado' });
+        }
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => console.log(`Servidor corriendo en: http://localhost:${PORT}`));
